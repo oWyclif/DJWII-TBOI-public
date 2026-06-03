0e980e3a4b70e38a8c5dced33d3167ef1eab5060
Wyclif ETEC <jorgewyclif@gmail.com>
2026-06-03 09:26:50 -0300

    Prep Ataque State - Comentários

--- Assets/Scripts/Boss/BossStatePrepAtaque.cs
+++ Assets/Scripts/Boss/BossStatePrepAtaque.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class BossStatePrepAtaque : BossBaseState
 {
+    // Pegamos o componente transform do boss, para medir e mudar sua velocidade ou posiçõa
     Transform bossTransform;
 
+    // Nessa parte definimos as nossas variáveis para manipular a escala do boss
     Vector3 startScale;
     Vector3 targetScale = Vector3.one * 5.0f;
     float t = 0f;
 
+    // E aqui vai nossas variáveis relacionadas ao player
     GameObject player; 
     Transform playerTransform;
+    
     public override void EnterState(BossStateManager boss){
         //Debug.Log("Boss Preparando Ataque");
 
@@ -19,17 +23,21 @@ public class BossStatePrepAtaque : BossBaseState
         playerTransform = player.GetComponent<Transform>(); // E definimos o playertransform,
         // a posição do player
 
+        // Encontramos o objeto boss e pegamos seu componente Transform
         bossTransform = GameObject.Find("boss").GetComponent<Transform>();
 
+        // Definimos a escala inicial do boss.
         startScale = bossTransform.localScale;
     }
 
     public override void UpdateState(BossStateManager boss){
         t += Time.deltaTime / 1f;
 
+        // Transformamos linearmente a escala do boss em todos os frames
         Vector3 newScale = Vector3.Lerp(startScale, targetScale, t);
         bossTransform.localScale = newScale;
 
+        // Até ela atingir 5, quando iniciamos a segunda parte do ataque, o ataque em si
         if (bossTransform.localScale.x == 5f)
         {
             boss.SwitchState(boss.AtaqueState);
